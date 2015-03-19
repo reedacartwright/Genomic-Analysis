644373024c6a896e7694fc9ff2818c6614d0a603
Reed A. Cartwright <cartwright@asu.edu>
2015-03-19 14:11:26 -0700

    ebola simulated data

---
+++ extra/ebolasim.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+art_illumina -1 data/EmpMiSeq250R1.txt -2 data/EmpMiSeq250R2.txt \
+	-rs 5842 -p -i ../data/ebola.fasta -l 100 -f 20 -m 300 -s 10 -sam -na \
+	-d ebola_sample_ -o ../data/ebola_sample_
